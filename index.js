7e280544e7bb57c95fb8f434c46a5c490e5706f4
Ebrusnev <evgenii.brusnev@gmail.com>
2017-04-02 17:20:04 -0500

    Almost there

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var parser = require('body-parser')
 var _ = require('underscore');
 var ejs = require('ejs');
+var fs = require('fs');
 
 var app = express();
 app.use(parser.urlencoded({extended: false}));
@@ -11,13 +12,22 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res){
-	res.render('index.ejs');
+	res.render('form.ejs');
 });
 
-app.post('/articles', function(req, res){
+app.post('/', function(req, res){
 	var userName = req.body.name;
 	var comment = req.body.comment;
-
+	if(userName.length < 6 || comment.length < 2){
+		res.send('Require more simbols');
+	}
+	if(!req.xhr){
+		res.json(req.body);
+		fs.writeFile('./data.txt', userName + " " + comment, function(){
+			console.log('The file is written!');
+			res.end('gg');
+		});
+	}
 });
 app.listen(3030, function() {
 	console.log('Server is listening on port 3030');
