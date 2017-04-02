223c5bd1b7272b55119caa4f9d390bed0347eefb
Ebrusnev <evgenii.brusnev@gmail.com>
2017-04-02 16:12:39 -0500

    index.js modified

--- index.js
+++ index.js
@@ -4,25 +4,16 @@ var _ = require('underscore');
 var ejs = require('ejs');
 
 var app = express();
-app.use(parser.urlencoded({extended: false}))
+app.use(parser.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
 
 app.get("/", function(req, res){
-	res.send('Hello There!')
+	res.render('index.ejs');
 });
 
-app.get('/articles', function(req, res){
-});
-
-app.get('/articles/:id', function(req, res){
-	var artcls = _.find(posted, function(elm) {
-		return req.params.id == elm.id;
-		res.send(artcls);
-	})
-});
 app.post('/articles', function(req, res){
 	var userName = req.body.name;
 	var comment = req.body.comment;
